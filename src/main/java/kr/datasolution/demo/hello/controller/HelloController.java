13539c9b1dbb430752775970fdc205e4adeb6064
조영석 <yscho@smilegate.com>
2020-10-06 16:28:48 +0900

    TEST_2

--- src/main/java/kr/datasolution/demo/hello/controller/HelloController.java
+++ src/main/java/kr/datasolution/demo/hello/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * TEST_1
+ * TEST_2
  */
 @Controller
 @RequestMapping(value = "/hello")
